279bde7ae3f626945861860092a03b761fc6a9b8
DarkMorford <DarkMorford@Gmail.com>
2017-01-30 18:15:43 -0800

    Read and print out header information.

--- theora/theoratest.cpp
+++ theora/theoratest.cpp
@@ -9,6 +9,13 @@ KOS_INIT_FLAGS(INIT_DEFAULT);
 extern uint8 romdisk[];
 KOS_INIT_ROMDISK(romdisk);
 
+ogg_stream_state theoraStream;
+ogg_stream_state vorbisStream;
+
+void queueDataPage(ogg_page *dataPage)
+{
+	ogg_stream_pagein(&theoraStream, dataPage);
+}
 
 size_t fillSyncBuffer(FILE *inFile, ogg_sync_state *sync)
 {
@@ -41,16 +48,15 @@ int main(int argc, char *argv[])
 	th_comment_init(&theoraComments);
 
 	th_setup_info *theoraSetup = NULL;
-	ogg_stream_state theoraStream;
 
 	// Parse the Ogg headers and find a Theora stream
 	bool parsingOggHeaders = true;
-	bool parsingTheoraHeaders = false;;
+	int parsingTheoraHeaders = 0;
 	bool foundTheora = false;
 	while (parsingOggHeaders)
 	{
 		// Load up the buffer with data
-		size_t bytesRead = fillSyncBuffer(videoFile, &syncState);
+		fillSyncBuffer(videoFile, &syncState);
 
 		// Get the next data page
 		ogg_page dataPage;
@@ -59,6 +65,9 @@ int main(int argc, char *argv[])
 			// Check to see if this is a header page
 			if (!ogg_page_bos(&dataPage))
 			{
+				// Put the page into its stream
+				queueDataPage(&dataPage);
+
 				// Headers are done
 				parsingOggHeaders = false;
 				break;
@@ -76,13 +85,14 @@ int main(int argc, char *argv[])
 			int gotPacket = ogg_stream_packetpeek(&codecTest, &headerPacket);
 
 			// Identify the codec
-			if ((gotPacket == 1) && !foundTheora && 
+			if ((gotPacket == 1) && !foundTheora &&
 				(parsingTheoraHeaders = th_decode_headerin(&theoraInfo, &theoraComments, &theoraSetup, &headerPacket)) >= 0)
 			{
 				dbglog(DBG_NOTICE, "Found Theora stream\n");
 				memcpy(&theoraStream, &codecTest, sizeof(codecTest));
 				foundTheora = true;
-				
+
+				// Already processed this, so just dump it
 				if (parsingTheoraHeaders)
 					ogg_stream_packetout(&theoraStream, NULL);
 			}
@@ -93,6 +103,46 @@ int main(int argc, char *argv[])
 			}
 		}
 	}
+
+	// Read the Theora headers
+	while (foundTheora && parsingTheoraHeaders)
+	{
+		int gotPacket;
+		ogg_packet dataPacket;
+		while (parsingTheoraHeaders && (gotPacket = ogg_stream_packetpeek(&theoraStream, &dataPacket)))
+		{
+			if (gotPacket < 0) continue;
+			parsingTheoraHeaders = th_decode_headerin(&theoraInfo, &theoraComments, &theoraSetup, &dataPacket);
+			if (parsingTheoraHeaders > 0)
+				// Dump the header packet
+				ogg_stream_packetout(&theoraStream, NULL);
+		}
+
+		ogg_page dataPage;
+		if (ogg_sync_pageout(&syncState, &dataPage) > 0)
+		{
+			queueDataPage(&dataPage);
+		}
+		else
+		{
+			fillSyncBuffer(videoFile, &syncState);
+		}
+	}
+
+	// Initialize the Theora decoder
+	th_dec_ctx *theoraDecoder;
+	if (foundTheora)
+	{
+		theoraDecoder = th_decode_alloc(&theoraInfo, theoraSetup);
+		dbglog(DBG_NOTICE, "Video is %lu x %lu, %lu / %lu fps\n", theoraInfo.frame_width, theoraInfo.frame_height,
+			theoraInfo.fps_numerator, theoraInfo.fps_denominator);
+	}
+	else
+	{
+		th_info_clear(&theoraInfo);
+		th_comment_clear(&theoraComments);
+	}
+	th_setup_free(theoraSetup);
 
 	// Shut things down
 	ogg_sync_clear(&syncState);
