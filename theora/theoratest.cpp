f1911ef25aab02d76c09e6c07e34dc956b3a5a77
DarkMorford <DarkMorford@Gmail.com>
2017-02-01 17:31:25 -0800

    Draw the first frame of the Theora stream.

--- theora/theoratest.cpp
+++ theora/theoratest.cpp
@@ -1,6 +1,8 @@
 #include <kos.h>
 
 #include <ogg/ogg.h>
+#include <plx/matrix.h>
+#include <plx/prim.h>
 #include <theora/codec.h>
 #include <theora/theoradec.h>
 
@@ -47,12 +49,34 @@ inline int nextPowerOfTwo(int n)
 	return n + 1;
 }
 
+vector_t verts[4] = {
+	{ -2.0f,  1.5f, 0.0f, 1.0f },
+	{ -2.0f, -1.5f, 0.0f, 1.0f },
+	{  2.0f,  1.5f, 0.0f, 1.0f },
+	{  2.0f, -1.5f, 0.0f, 1.0f }
+};
+
 int main(int argc, char *argv[])
 {
 	dbglog_set_level(DBG_INFO);
 	dbglog(DBG_NOTICE, "%s\n", th_version_string());
 	
 	pvr_init_defaults();
+	plx_mat3d_init();
+	
+	pvr_set_bg_color(0.2f, 0.2f, 0.2f);
+	
+	plx_mat3d_mode(PLX_MAT_PROJECTION);
+	plx_mat3d_identity();
+	plx_mat3d_perspective(60.0f, 640.0f / 480.0f, 0.1f, 100.0f);
+
+	plx_mat3d_mode(PLX_MAT_MODELVIEW);
+	plx_mat3d_identity();
+
+	point_t cameraPosition = { 0.0f, 0.0f,  5.0f, 1.0f };
+	point_t cameraTarget   = { 0.0f, 0.0f,  0.0f, 1.0f };
+	vector_t cameraUp      = { 0.0f, 1.0f,  0.0f, 0.0f };
+	plx_mat3d_lookat(&cameraPosition, &cameraTarget, &cameraUp);
 
 	// Open the video file
 	FILE *videoFile = fopen("/rd/320x240.ogg", "rb");
@@ -186,13 +210,54 @@ int main(int argc, char *argv[])
 	// Allocate a memory buffer for the YUV422 data
 	pvr_ptr_t textureData = pvr_mem_malloc(texBufferSize);
 	unsigned char *yuvData = reinterpret_cast<unsigned char*>(memalign(32, texBufferSize));
-	sq_set32(yuvData, 0x80008000, texBufferSize);
+	sq_set32(yuvData, 0x00800080, texBufferSize);
 
 	// TODO: Fix this function to consider the texture width
 	convertYUV420pTo422(yuvData, videoFrame, textureWidth);
 	
 	// Copy the YUV422 data to the PVR's memory
 	sq_cpy(textureData, yuvData, texBufferSize);
+	
+	pvr_poly_cxt_t stripContext;
+	pvr_poly_cxt_txr(&stripContext, PVR_LIST_OP_POLY, PVR_TXRFMT_YUV422 | PVR_TXRFMT_NONTWIDDLED,
+		textureWidth, textureHeight, textureData, PVR_FILTER_NONE);
+	stripContext.gen.culling = PVR_CULLING_CW;
+	pvr_poly_hdr_t stripHeader;
+	pvr_poly_compile(&stripHeader, &stripContext);
+	
+	plx_mat_identity();
+	plx_mat3d_apply_all();
+		
+	vector_t transformedVerts[4];
+	plx_mat_transform(verts, transformedVerts, 4, 16);
+	
+	float rightLimit = 320.0f / (float)textureWidth;
+	float bottomLimit = 240.0f / (float)textureHeight;
+	
+	bool exitProgram = false;
+	while(!exitProgram)
+	{
+		plx_mat_identity();
+		plx_mat3d_apply_all();
+		
+		pvr_wait_ready();
+		pvr_scene_begin();
+		pvr_list_begin(PVR_LIST_OP_POLY);
+		
+		pvr_prim(&stripHeader, sizeof(stripHeader));
+		plx_vert_ffp(PVR_CMD_VERTEX,     transformedVerts[0].x, transformedVerts[0].y, transformedVerts[0].z, 1, 1, 1, 1, 0, 0);
+		plx_vert_ffp(PVR_CMD_VERTEX,     transformedVerts[1].x, transformedVerts[1].y, transformedVerts[1].z, 1, 1, 1, 1, 0, bottomLimit);
+		plx_vert_ffp(PVR_CMD_VERTEX,     transformedVerts[2].x, transformedVerts[2].y, transformedVerts[2].z, 1, 1, 1, 1, rightLimit, 0);
+		plx_vert_ffp(PVR_CMD_VERTEX_EOL, transformedVerts[3].x, transformedVerts[3].y, transformedVerts[3].z, 1, 1, 1, 1, rightLimit, bottomLimit);
+		
+		pvr_list_finish();
+		pvr_scene_finish();
+		
+		maple_device_t *controller = maple_enum_type(0, MAPLE_FUNC_CONTROLLER);
+		cont_state_t *controllerState = reinterpret_cast<cont_state_t*>(maple_dev_status(controller));
+		if (controllerState->buttons & CONT_START)
+			exitProgram = true;
+	}
 	
 	// Shut things down
 	pvr_mem_free(textureData);
