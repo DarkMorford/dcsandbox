da3956718b391fc4b014db899dc25be1c1923d23
DarkMorford <DarkMorford@users.noreply.github.com>
2017-02-10 08:07:41 -0800

    Function reuse is good, m'kay?

--- theora/threadFuncs.cpp
+++ theora/threadFuncs.cpp
@@ -17,19 +17,12 @@ void* fileReaderThread(void *threadParams)
 		}
 		
 		// Read data from file
-		char *dataBuffer = ogg_sync_buffer(params->mediaSync, params->bufferSize);
-		size_t bytesRead = fread(dataBuffer, 1, params->bufferSize, params->mediaFile);
-		ogg_sync_wrote(params->mediaSync, bytesRead);
+		size_t bytesRead = fillSyncBuffer(params->mediaFile, *(params->mediaSync), params->bufferSize);
 		if (bytesRead == 0)
 			return NULL;
 		
 		// Pump it into the streams
-		ogg_page dataPage;
-		while(ogg_sync_pageout(params->mediaSync, &dataPage) == 1)
-		{
-			int pageSerial = ogg_page_serialno(&dataPage);
-			ogg_stream_pagein(&(*params->oggStreams)[pageSerial], &dataPage);
-		}
+		flushSyncBuffer(*(params->mediaSync), *(params->oggStreams));
 		
 		// Signal that we've added data to the streams
 		*params->streamNeedsData = false;
